ed45ca642c63ed289ee50258260f0f783efc8e4c
Jeremy Zilar <jeremyzilar@gmail.com>
2014-05-28 17:21:24 -0400

    adding in Google Tag Manager

--- header.php
+++ header.php
@@ -75,6 +75,16 @@
 <?php $slug = get_the_slug(); ?>
 <body <?php body_class($slug); ?> data-spy="scroll" data-target="#sidenav">
 
+  <!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N8RQ8V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N8RQ8V');</script>
+<!-- End Google Tag Manager -->
+
   <?php include 'includes/login-bar.php'; ?>
   
   <?php // include 'includes/inbox.php'; ?>
