e3b029ebebc5b41eb7a9ca8cb620233410be68e2
Jeremy Zilar <jeremyzilar@gmail.com>
2014-09-30 11:01:53 -0400

    add children column to users.php

---
+++ functions/users.php
@@ -0,0 +1,41 @@
+<?php
+
+
+// Add a column to the User Table
+function test_modify_user_table( $column ) {
+    $column['children'] = 'Children';
+    return $column;
+}
+add_filter( 'manage_users_columns', 'test_modify_user_table' );
+
+function get_child_name($child_id){
+	$child = get_post( $child_id );
+	$child_name = (!empty($child) ? $child->post_title : '');
+	// $child_name = $child->post_title;
+	return $child_name;
+}
+
+function test_modify_user_table_row( $val, $column_name, $user_id ) {
+    $user = get_userdata( $user_id );
+    $meta = get_user_meta($user->ID);
+    
+    $child1 = (!empty($meta['child1']['0']) ? get_child_name($meta['child1']['0']) . '<br/>' : '');
+    $child2 = (!empty($meta['child2']['0']) ? get_child_name($meta['child2']['0']) . '<br/>' : '');
+    $child3 = (!empty($meta['child3']['0']) ? get_child_name($meta['child3']['0']) . '<br/>' : '');
+    $child4 = (!empty($meta['child4']['0']) ? get_child_name($meta['child4']['0']) . '<br/>' : '');
+    $children = <<<EOF
+    	$child1
+    	$child2
+    	$child3
+    	$child4
+EOF;
+    switch ($column_name) {
+        case 'children' :
+            // return '$children';
+            return $children;
+            break;
+        default:
+    }
+    return $return;
+}
+add_filter( 'manage_users_custom_column', 'test_modify_user_table_row', 10, 6 );
