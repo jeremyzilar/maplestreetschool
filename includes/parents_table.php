449cce301b15807065257d6bdc3e9ec6ab654fed
Jeremy Zilar <jeremyzilar@gmail.com>
2014-10-01 00:30:05 -0400

    cleaning up

--- includes/parents_table.php
+++ includes/parents_table.php
@@ -1,20 +1,4 @@
-<?php show_families(); ?>
-
-<table id="roster" class="table">
-	<thead>
-		<tr>
-			<th>Child Name</th>
-			<th>Class</th>
-			<th>Committee</th>
-			<th>Parent Name</th>
-			<th>Phone</th>
-			<th>Work Phone</th>
-		</tr>			
-	</thead>
-	<tbody>
-<?php
-	
-
+<?php 
 function get_families(){
 	$student_args = array(
 	  'post_type' => 'student',
@@ -85,7 +69,7 @@ function get_families(){
 	wp_reset_query();  // Restore global post data stomped by the_post().
 }
 
-function show_families(){
+function show_family_cards(){
 	$families = get_families();
 	$i = 0;
 	foreach ($families as $family) {
@@ -137,51 +121,71 @@ EOF;
 	}
 }
 
+function show_family_table(){
 
+	echo <<< EOF
+	<table id="roster" class="table">
+		<thead>
+			<tr>
+				<th>Child Name</th>
+				<th>Class</th>
+				<th>Days</th>
+			</tr>			
+		</thead>
+		<tbody>
+EOF;
+	
+	$families = get_families();
+	foreach ($families as $family) {
+		$child_name = $family['child_name'];
+		$image = $family['image'];
+		$classroom = $family['classroom'];
+		$birthday = $family['birthday'];
+		$age = $family['age'];
+		$days = $family['days'];
+		$day_type = $family['day_type'];
 
-
-
-
-
-	foreach ( $blogusers as $user ) {
-		// print_r($user);
-		$meta = get_user_meta($user->ID);
-		// print_r($meta);
-		$user_id = esc_html( $user->ID );
-
-		$first_name = esc_html( $meta['first_name']['0'] );
-		$last_name = esc_html( $meta['last_name']['0'] );
-		$bio = esc_html( $meta['description']['0'] );
-		$user_email = '<a href="mailto:'.esc_html( $user->user_email ).'">'.esc_html( $user->user_email ).'</a>';
-		$tel = (!empty($meta['tel']['0']) ? $meta['tel']['0'] : '');
-		$wktel = (!empty($meta['wktel']['0']) ? $meta['wktel']['0'] : '');
-		$street_address = (!empty($meta['street_address']['0']) ? $meta['street_address']['0'] : '');
-		$locality = (!empty($meta['locality']['0']) ? $meta['locality']['0'] : '');
-		$region = (!empty($meta['region']['0']) ? $meta['region']['0'] : '');
-		$postal_code = (!empty($meta['postal_code']['0']) ? $meta['postal_code']['0'] : '');
-		$committee = (!empty($meta['committee']['0']) ? $meta['committee']['0'] : '');
-		$partner = (!empty($meta['partner']['0']) ? $meta['partner']['0'] : '');
-		$class = (!empty($meta['class']['0']) ? '<h6>' . $meta['class']['0']. '</h6>': '');
-		$days = (!empty($meta['days']['0']) ? $meta['days']['0'] : '');
-		$child_id = (!empty($meta['child1']['0']) ? $meta['child1']['0'] : '');
-		if (!empty($child_id)) {
-			$child = get_post( $child_id );
-			$child_name = $child->post_title;
-		} else {
-			$child_name = '';
+		foreach ($family['parents'] as $parent) {
+			// print_r($parent);
+			$firstname = $parent['firstname'];
+			$lastname = $parent['lastname'];
+			$user_email = $parent['user_email'];
+			$tel = $parent['tel'];
+			$wktel = (!empty($parent['wktel']) ? '/ wk: '.$parent['wktel'] : '');
+			$street_address = (!empty($parent['street_address']) ? $parent['street_address'] .',' : '');
+			$locality = (!empty($parent['locality']) ? $parent['locality'] .',' : '');
+			$region = $parent['region'];
+			$postal_code = $parent['postal_code'];
+			$committee = $parent['committee'];
+			echo <<< EOF
+			<div class="parent">
+				<h5>$firstname $lastname</h5>
+				<p>$user_email $tel $wktel</p>
+				<p>$street_address $locality $region $postal_code</p>
+			</div>
+EOF;
 		}
+		
 		echo <<< EOF
 		<tr>
 			<td>$child_name</td>
-			<td>$class</td>
-			<td>$committee</td>
-			<td>$first_name $last_name</td>
-			<td>$tel</td>
-			<td>$wktel</td>
+			<td>$classroom</td>
+			<td>$day_type / $days</td>
 		</tr>
 EOF;
 	}
+
+	echo <<< EOF
+		</tbody>
+	</table>
+EOF;
+
+}
+
+
+show_family_cards();
+show_family_table();
+
+	
 ?>
 
-	</tbody>
-</table>
