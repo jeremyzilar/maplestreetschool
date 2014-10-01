e0252605cfe74e926f3d28eba9d8a2769f8f98ea
Jeremy Zilar <jeremyzilar@gmail.com>
2014-10-01 02:10:30 -0400

    removing classifiesds list

--- archive-class_blog.php
+++ archive-class_blog.php
@@ -33,26 +33,6 @@ Template Name: Coop Docs / Handbook Index
     
 		<?php if ( is_user_logged_in() ) { ?>
 		  
-			<!-- Classifieds -->
-			<div class="classifieds">
-			  <h4>Classifieds</h4>
-			  <p class="helper">Want to post an item?<br /><a href="<?php echo get_post_type_archive_link( 'classified' ); ?>">E-mail us</a>.</p>
-			  <?php
-			  $args = array(
-			  	'post_type' => array('classified'),
-			  	'post_status' => 'publish',
-			  	'posts_per_page' => 5
-			  );
-			  $the_query = new WP_Query( $args );
-			  if ($the_query -> have_posts()) {
-			    while ( $the_query->have_posts() ) : $the_query->the_post();
-			      include('templates/classifieds.php');
-			    endwhile; ?>
-			      <p><a class="btn btn-small btn-primary" href="<?php echo get_post_type_archive_link( 'classified' ); ?>">See all</a></p>
-			    <?php
-			  }?>
-			</div>
-      
       <div class="row">
         <div id="blogposts" class="span10">
 					
